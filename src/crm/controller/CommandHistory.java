fc4cdd95fb3b0cbf3e7ea8e0921fffa3aaed3afa
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-12-01 17:15:56 -0500

    updated CommandHistory to implement Command and include history stack

--- src/crm/controller/CommandHistory.java
+++ src/crm/controller/CommandHistory.java
@@ -1,4 +1,20 @@
 package crm.controller;
 
-public class CommandHistory {
+import crm.controller.command.Command;
+
+import java.util.Stack;
+
+public class CommandHistory implements Command {
+    private Stack<Command> history = new Stack<Command>();
+
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
 }
