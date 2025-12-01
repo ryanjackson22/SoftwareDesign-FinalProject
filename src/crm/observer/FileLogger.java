3aaea46686b98f7ecb02a6d7711c4dbbd74878fa
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-12-01 17:34:53 -0500

    added members and methods to FileLogger and SalesLogger

--- src/crm/observer/FileLogger.java
+++ src/crm/observer/FileLogger.java
@@ -3,8 +3,14 @@ package crm.observer;
 import crm.observer.event.EventType;
 
 public class FileLogger implements CRMObserver {
+    private String filePath;
+
     @Override
     public void onEvent(EventType eventType) {
+        // stub
+    }
 
+    public void writeToFile(String filePath) {
+        // stub
     }
 }
--- src/crm/observer/SalesLogger.java
+++ src/crm/observer/SalesLogger.java
@@ -3,8 +3,14 @@ package crm.observer;
 import crm.observer.event.EventType;
 
 public class SalesLogger implements CRMObserver {
+    private String filePath;
+
     @Override
     public void onEvent(EventType eventType) {
+        // stub
+    }
 
+    public void writeToFile(String filePath) {
+        // stub
     }
 }
