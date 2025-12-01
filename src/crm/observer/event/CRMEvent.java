2c471db422adb038824d09c2d25976b07a50e434
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-12-01 17:28:36 -0500

    added members and methods to CRMEvent.java

--- src/crm/observer/event/CRMEvent.java
+++ src/crm/observer/event/CRMEvent.java
@@ -1,4 +1,17 @@
 package crm.observer.event;
 
+import java.util.Date;
+
 public class CRMEvent {
+    private EventType eventType;
+    private Date timestamp;
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public String toString() {
+        return "";
+        // stub
+    }
 }
