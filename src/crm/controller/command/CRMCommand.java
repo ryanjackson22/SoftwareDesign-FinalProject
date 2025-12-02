3b02c48650b460a3fef82884bd95e87fbabfa930
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-02 04:59:54 -0500

    Remove accessors from interface members

--- src/crm/controller/command/CRMCommand.java
+++ src/crm/controller/command/CRMCommand.java
@@ -3,6 +3,6 @@ package crm.controller.command;
 import contact.Contact;
 
 public interface CRMCommand {
-    public void execute(Contact contact);
-    public void undo();
+    void execute(Contact contact);
+    void undo();
 }
