4aa3da2b3d1839939e5a28443f66a058ec9db569
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-01 20:47:02 -0500

    Add accessors for customer fields

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -15,4 +15,52 @@ public abstract class Customer {
     public void contact(String message) {
         preferredContactMethod.send(this,message);
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getLastContact() {
+        return lastContact;
+    }
+
+    public void setLastContact(Date lastContact) {
+        this.lastContact = lastContact;
+    }
+
+    public NotificationStrategy getPreferredContactMethod() {
+        return preferredContactMethod;
+    }
+
+    public void setPreferredContactMethod(NotificationStrategy preferredContactMethod) {
+        this.preferredContactMethod = preferredContactMethod;
+    }
 }
