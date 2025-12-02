32d3d9b3e4a76b535b7d7d98b5c0443f4584a9e6
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-01 21:01:32 -0500

    Add implementations for the different notification strategies

--- src/notification/PushNotification.java
+++ src/notification/PushNotification.java
@@ -5,6 +5,10 @@ import customer.Customer;
 public class PushNotification implements NotificationStrategy {
     @Override
     public void send(Customer customer, String message) {
-        // stub
+        System.out.println(
+            "Sending push notification." +
+            "  Customer: " + customer.getName() +
+            "  Message: " + message
+        );
     }
 }
--- src/notification/SMSNotification.java
+++ src/notification/SMSNotification.java
@@ -5,6 +5,10 @@ import customer.Customer;
 public class SMSNotification implements NotificationStrategy {
     @Override
     public void send(Customer customer, String message) {
-        // stub
+        System.out.println(
+                "Sending SMS notification." +
+                        "  Customer: " + customer.getName() +
+                        "  Message: " + message
+        );
     }
 }
