2b11e54bad1e4e1d4b211b15df5fd221bfd344b5
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-12-05 17:27:01 -0500

    Moved command pattern code into CreateContactCommand.java

--- src/crm/controller/CRMController.java
+++ src/crm/controller/CRMController.java
@@ -1,5 +1,7 @@
 package crm.controller;
 
+import crm.controller.command.CRMCommand;
+import crm.controller.command.CreateContactCommand;
 import crm.observer.CRMObserver;
 import customer.Customer;
 import customer.LeadCustomer;
@@ -20,35 +22,10 @@ public class CRMController {
     }
 
     public void createContact() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Contact Type: ");
-        System.out.println("1. Customer Lead");
-        System.out.println("2. Current Customer");
-        System.out.println("3. VIP Customer");
-        String customerType = scanner.nextLine();
+        CRMCommand createContact = new CreateContactCommand();
+        createContact.execute();
 
-        System.out.println();
-        System.out.println("============ Contact Info. ============");
-        System.out.print("Full Name: ");
-        String fullName = scanner.nextLine();
-
-        System.out.print("Email Address: ");
-        String email = scanner.nextLine();
-
-        System.out.print("Phone Number: ");
-        String phone = scanner.nextLine();
-
-        switch (customerType) {
-            case "1":
-                customers.add(new LeadCustomer(fullName, email, phone));
-                break;
-            case "2":
-                customers.add(new RegularCustomer(fullName, email, phone));
-                break;
-            case "3":
-                customers.add(new VIPCustomer(fullName, email, phone));
-                break;
-        }
+        commandHistory.lastCommand(createContact);
     }
 
     public void deleteContact(Customer customer) {
