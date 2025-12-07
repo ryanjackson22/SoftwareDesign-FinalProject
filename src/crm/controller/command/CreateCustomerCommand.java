8f1bb8239311ab0cd21eb159adfbfbde42a68ba2
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-07 16:13:48 -0500

    Add functionality for CreateCustomerCommand

--- src/crm/controller/command/CreateCustomerCommand.java
+++ src/crm/controller/command/CreateCustomerCommand.java
@@ -1,17 +1,32 @@
 package crm.controller.command;
 
-import customer.*;
+import crm.observer.event.EventType;
+import customer.Customer;
+import customer.LeadCustomer;
+import customer.RegularCustomer;
+import customer.VIPCustomer;
+import repository.CustomerRepository;
 
 import java.util.Scanner;
+import java.util.Stack;
 
-public class CreateContactCommand implements CRMCommand {
+public class CreateCustomerCommand implements CRMCommand {
+    private final String name = "Create Customer";
+    private final EventType eventType = EventType.CUSTOMER_CREATED;
+
+    private final CustomerRepository customerRepository;
+    private Stack<Customer> createdCustomers;
+
+    public CreateCustomerCommand(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public void execute() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Contact Type: ");
-        System.out.println("1. Customer Lead");
-        System.out.println("2. Current Customer");
+        System.out.println("Customer Type: ");
+        System.out.println("1. Lead Customer");
+        System.out.println("2. Regular Customer");
         System.out.println("3. VIP Customer");
         String customerType = scanner.nextLine();
 
@@ -26,21 +41,36 @@ public class CreateContactCommand implements CRMCommand {
         System.out.print("Phone Number: ");
         String phone = scanner.nextLine();
 
+        Customer newCustomer;
         switch (customerType) {
             case "1":
-//                customers.add(new LeadCustomer(fullName, email, phone));
+                newCustomer = new LeadCustomer(fullName, email, phone);
                 break;
+            default:
             case "2":
-//                customers.add(new RegularCustomer(fullName, email, phone));
+                newCustomer = new RegularCustomer(fullName, email, phone);
                 break;
             case "3":
-//                customers.add(new VIPCustomer(fullName, email, phone));
+                newCustomer = new VIPCustomer(fullName, email, phone);
                 break;
         }
+
+        createdCustomers.push(newCustomer);
+        customerRepository.createCustomer(newCustomer);
     }
 
     @Override
     public void undo() {
+        customerRepository.deleteCustomer(createdCustomers.pop());
+    }
 
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public EventType getEventType() {
+        return eventType;
     }
 }
