36791db33154a32aebe2d7a88c30992f3b6becaf
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-07 16:46:25 -0500

    Add MakeSaleCommand

--- src/crm/controller/command/MakeSaleCommand.java
+++ src/crm/controller/command/MakeSaleCommand.java
@@ -1,13 +1,47 @@
 package crm.controller.command;
 
+import crm.observer.event.EventType;
+import customer.Customer;
+import repository.CustomerRepository;
+
+import java.util.Scanner;
+import java.util.Stack;
+
 public class MakeSaleCommand implements CRMCommand {
+    private final String name = "Make Sale";
+    private final EventType eventType = EventType.SALE_MADE;
+
+    private final CustomerRepository customerRepository;
+    private Stack<Customer> customerSales;
+
+    public MakeSaleCommand(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Customer Id: ");
+        int customerId = Integer.parseInt(scanner.nextLine());
 
+        Customer saleCustomer = customerRepository.getCustomerFromId(customerId);
+        customerSales.push(saleCustomer);
+        System.out.println("Made sale with " + saleCustomer.getName());
     }
 
     @Override
     public void undo() {
+        Customer saleCustomer = customerSales.pop();
+        System.out.println("Unmade sale with " + saleCustomer.getName());
+    }
 
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public EventType getEventType() {
+        return eventType;
     }
 }
