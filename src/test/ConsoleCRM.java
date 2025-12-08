68697a2db4e37faffb6dcff117041d3165d9f354
ryanbuck182 <ryanbuck182@gmail.com>
2025-12-07 20:35:09 -0500

    Change ConsoleCRM flow
    A little janky but can be polished later

--- src/test/ConsoleCRM.java
+++ src/test/ConsoleCRM.java
@@ -1,43 +1,69 @@
 package test;
 
-
 import crm.controller.CRMController;
-import customer.Customer;
-import customer.LeadCustomer;
+import customer.*;
+import crm.controller.command.*;
+import repository.CustomerRepository;
+import repository.InMemoryCustomerRepository;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleCRM {
     public static void main(String[] args) {
+        clear();
         Scanner scanner = new Scanner(System.in);
 
+        CustomerRepository customerRepository = new InMemoryCustomerRepository();
+        customerRepository.createCustomer(new RegularCustomer("John Brown", "john.brown@tomato.mail", "123-123-1234"));
+        customerRepository.createCustomer(new RegularCustomer("Alice Sharp", "alice.sharp@olive.mail", "987-987-9876"));
+        customerRepository.createCustomer(new LeadCustomer("President Greeble", "greeble@whitehouse.gov", "111-222-3333"));
+        customerRepository.createCustomer(new VIPCustomer("Big Tim", "bigtim123@hotmail.com", "123-456-7890"));
+        customerRepository.createCustomer(new LostCustomer("Old Man Jenkins", "jenkins@aol.com", "321-654-0987"));
+
         CRMController crm = new CRMController();
-        crm.createContact(new LeadCustomer("John Doe", "john.doe@gmail.com", "(603)555-1234"));
 
+        CRMCommand createCustomerCommand = new CreateCustomerCommand(customerRepository);
+        CRMCommand updateCustomerCommand = new UpdateCustomerCommand(customerRepository);
+        CRMCommand deleteCustomerCommand = new DeleteCustomerCommand(customerRepository);
+        CRMCommand makeSaleCommand = new MakeSaleCommand(customerRepository);
 
-        System.out.println("=========== Console CRM ===========");
-        System.out.println("1. Manage Contacts");
-        System.out.println("2. View Analytics");
-        String input = scanner.nextLine();
-        if (input.equals("1")) {
-            clear();
-            System.out.println("========= Contact Management ===========");
-            System.out.println("1. Create Contact");
-            System.out.println("2. Update Contact");
-            System.out.println("3. Delete Contact");
-            System.out.println("4. Search Contact");
-            input = scanner.nextLine();
+        crm.addCommand(createCustomerCommand);
+        crm.addCommand(updateCustomerCommand);
+        crm.addCommand(deleteCustomerCommand);
+        crm.addCommand(makeSaleCommand);
 
-            clear();
-            if (input.equals("1")) {
-                crm.createContact();
+        while (true) {
+            System.out.println("========= CRM Commands ===========");
+            List<String> commandNames =  crm.getCommandListing();
+
+            int i;
+            for (i = 0; i < crm.getCommandListing().size(); i++) {
+                System.out.printf("%d. %s%n", i+1, commandNames.get(i));
             }
 
-            crm.printCustomers();
+            System.out.printf("%d. %s%n", i+1, "Print Customers");
+            System.out.printf("%d. %s%n", i+2, "Undo");
+            System.out.printf("%d. %s%n", i+3, "Exit");
+            System.out.print("Your choice: ");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            clear();
+            if (input < i+1) {
+                crm.executeCommand(input - 1);
+            } else if (input == i+1) {
+                customerRepository.getAllCustomers().forEach(System.out::println);
+                System.out.println("Press enter to continue...");
+                scanner.nextLine();
+            } else if (input == i+2) {
+                crm.undoCommand();
+            } else if (input == i+3) {
+                break;
+            }
+            clear();
         }
 
         scanner.close();
-
-
     }
 
     public static void clear() {
